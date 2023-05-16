3dab6e00940b2ee97dfa4b22c289ce3ebf7510f4
Tristan22400 <tristanmartin22400@gmail.com>
2023-05-16 16:49:17 +0200

    feat: add new challenge

---
+++ src/ProxyDeploiment.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.6.0) (proxy/Proxy.sol)
+
+pragma solidity ^0.8.0;
+
+// These contracts are examples of contracts with bugs and vulnerabilities to practice your hacking skills.
+// DO NOT USE THEM OR GET INSPIRATION FROM THEM TO MAKE CODE USED IN PRODUCTION.
+// You are required to find vulnerabilities and problem in the contract.
+// The goal is to update the Proxy contract from the TokenV1 to the TokenV2 contract.
+// You have on this file three contracts with one proxy contract and two implementation contracts. The owner of these contracts wants to change the implementation of his proxy from TokenV1 to TokenV2.
+// The purpose of this exercise is to give the method to change the implementation of the proxy and to find if there are errors.
+
+contract Proxy {
+    address internal admin;
+    address internal implementation;
+
+    constructor() {
+        admin = msg.sender;
+    }
+    /**
+     * @dev Delegates the current call to `implementation`.
+     *
+     * This function does not return to its internal call site, it will return directly to the external caller.
+     */
+
+    function _delegate(address implementationUsed) internal virtual {
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(gas(), implementationUsed, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+
+    /**
+     * @dev Returns the current implementation address.
+     */
+    function getImplementation() external view returns (address) {
+        return implementation;
+    }
+
+    /**
+     * @dev Stores a new address in the EIP1967 implementation slot.
+     */
+    function setImplementation(address newImplementation) external {
+        require(newImplementation.code.length > 0, "new implementation is not a contract");
+        require(admin == msg.sender);
+        implementation = newImplementation;
+    }
+    /**
+     * @dev Delegates the current call to the address returned by `_implementation()`.
+     *
+     * This function does not return to its internal call site, it will return directly to the external caller.
+     */
+
+    function _fallback() internal virtual {
+        _delegate(implementation);
+    }
+
+    /**
+     * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if no other
+     * function in the contract matches the call data.
+     */
+    fallback() external payable virtual {
+        _fallback();
+    }
+
+    /**
+     * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if call data
+     * is empty.
+     */
+    receive() external payable virtual {
+        _fallback();
+    }
+}
+
+contract TokenV1 {
+    uint256 totalSupply;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    event Transfer(address from, address to, uint256 amount);
+    event Approval(address owner, address spender, uint256 amount);
+
+    function transfer(address to, uint256 amount) external returns (bool success) {
+        require(balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool success) {
+        require(allowed[from][msg.sender] >= amount);
+        allowed[from][msg.sender] -= amount;
+        balances[from] -= amount;
+        balances[to] += amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 tokens) external returns (bool success) {
+        require(allowed[msg.sender][spender] == 0, "");
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+}
+
+contract TokenV2 {
+    address owner;
+    uint256 totalSupply;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    event Transfer(address from, address to, uint256 amount);
+    event Approval(address owner, address spender, uint256 amount);
+    event Mint(address minter, uint256 amount);
+    event Redeem(address redeemer, uint256 amount);
+
+    function mint(uint256 amount) external returns (bool) {
+        require(msg.sender == owner || allowed[owner][msg.sender] >= amount);
+        balances[msg.sender] += amount;
+        if (msg.sender != owner) {
+            allowed[owner][msg.sender] -= amount;
+        }
+        emit Mint(msg.sender, amount);
+        return true;
+    }
+
+    function redeem(uint256 amount) external returns (bool) {
+        require(balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool success) {
+        require(balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 tokens) external returns (bool success) {
+        require(allowed[msg.sender][spender] == 0, "");
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+}
