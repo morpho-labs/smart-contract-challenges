8d07494a0495df50c692ae60836923ab168f26d1
makcandrov <makcandrov@proton.me>
2023-05-16 02:16:12 -0700

    ex5: refactor

--- src/Hack.sol
+++ src/Hack.sol
@@ -109,54 +109,23 @@ contract Vault {
 
 /// Exercice 5 ///
 
-/// @dev You choose Head or Tail and send 1 ETH.
-///      The next party send 1 ETH and try to guess what you chose.
-///      If it succeed it gets 2 ETH, else you get 2 ETH.
-contract HeadTail {
-    address payable public partyA;
-    address payable public partyB;
-    bytes32 public commitmentA;
-    bool public chooseHeadB;
-    uint256 public timeB;
+/// @dev Contract for locking and unlocking funds using a commitment and password.
+contract Locker {
+    bytes32 commitment;
 
-    /// @dev Constructor, commit head or tail.
-    /// @param _commitmentA is keccak256(abi.encode(chooseHead,randomNumber));
-    constructor(bytes32 _commitmentA) payable {
-        require(msg.value == 1 ether);
-
-        commitmentA = _commitmentA;
-        partyA = payable(msg.sender);
+    /// @dev Locks the funds by setting the commitment.
+    /// @param _commitment The commitment to lock the funds.
+    function lock(bytes32 _commitment) external payable {
+        require(_commitment != bytes32(0), "Invalid commitment");
+        commitment = _commitment;
     }
 
-    /// @dev Guesses the choice of party A.
-    /// @param _chooseHead True if the guess is head, false otherwise.
-    function guess(bool _chooseHead) public payable {
-        require(msg.value == 1 ether);
-        require(partyB == address(0));
-
-        chooseHeadB = _chooseHead;
-        timeB = block.timestamp;
-        partyB = payable(msg.sender);
-    }
-
-    /// @dev Reveals the commited value and send ETH to the winner.
-    /// @param _chooseHead True if head was chosen.
-    /// @param _randomNumber The random number chosen to obfuscate the commitment.
-    function resolve(bool _chooseHead, uint256 _randomNumber) public {
-        require(msg.sender == partyA);
-        require(keccak256(abi.encode(_chooseHead, _randomNumber)) == commitmentA);
-        require(address(this).balance >= 2 ether);
-
-        if (_chooseHead == chooseHeadB) partyB.transfer(2 ether);
-        else partyA.transfer(2 ether);
-    }
-
-    /// @dev Time out party A if it takes more than 1 day to reveal.
-    ///      Sends ETH to party B.
-    function timeOut() public {
-        require(block.timestamp > timeB + 1 days);
-        require(address(this).balance >= 2 ether);
-        partyB.transfer(2 ether);
+    /// @dev Unlocks the funds by comparing the provided password with the commitment.
+    /// @param _password The password to unlock the funds.
+    function unlock(string calldata _password) external {
+        require(keccak256(abi.encode(_password)) == commitment, "Invalid password");
+        (bool success,) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
     }
 }
 
