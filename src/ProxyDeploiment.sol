9ec6115dd20719ff962a8fdeeea227d5221bd1f8
Tristan22400 <tristanmartin22400@gmail.com>
2023-05-17 10:58:50 +0200

    fix: add ownable inheritance

--- src/ProxyDeploiment.sol
+++ src/ProxyDeploiment.sol
@@ -121,8 +121,53 @@ contract TokenV1 {
     }
 }
 
-contract TokenV2 {
-    address owner;
+abstract contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        transferOwnership(msg.sender);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == msg.sender, "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+contract TokenV2 is Ownable {
     uint256 totalSupply;
     mapping(address => uint256) balances;
     mapping(address => mapping(address => uint256)) allowed;
@@ -132,15 +177,11 @@ contract TokenV2 {
     event Mint(address minter, uint256 amount);
     event Redeem(address redeemer, uint256 amount);
 
-    constructor() {
-        owner = msg.sender;
-    }
-
     function mint(uint256 amount) external returns (bool) {
-        require(msg.sender == owner || allowed[owner][msg.sender] >= amount);
+        require(msg.sender == owner() || allowed[owner()][msg.sender] >= amount);
         balances[msg.sender] += amount;
-        if (msg.sender != owner) {
-            allowed[owner][msg.sender] -= amount;
+        if (msg.sender != owner()) {
+            allowed[owner()][msg.sender] -= amount;
         }
         emit Mint(msg.sender, amount);
         return true;
