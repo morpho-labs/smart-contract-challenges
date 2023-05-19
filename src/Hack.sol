84b4b3b60c598fae6a2b83a1d0a8fc4f0777da50
makcandrov <makcandrov@proton.me>
2023-05-19 00:01:42 -0700

    ex16: fix comment style

--- src/Hack.sol
+++ src/Hack.sol
@@ -699,32 +699,25 @@ contract WinnerTakesAll {
 
 /* Exercise 16 */
 
-/**
- * @title RewardsDistributor
- * @dev A contract for distributing rewards using Merkle proofs.
- */
+/// @dev A contract for distributing rewards using Merkle proofs.
 contract RewardsDistributor {
     uint256 public constant REWARD_AMOUNT = 1 ether;
     bytes32 public immutable ROOT;
 
     mapping(bytes32 node => bool) public claimed;
 
-    /**
-     * @notice Assumes that the deployer has provided a valid root hash, and sent the correct amount of ETH with the deployment.
-     * @param root The root hash of the Merkle tree used for reward distribution.
-     */
+    /// @notice Assumes that the deployer has provided a valid root hash, and sent the correct amount of ETH with the deployment.
+    /// @param root The root hash of the Merkle tree used for reward distribution.
     constructor(bytes32 root) payable {
         ROOT = root;
     }
 
-    /**
-     * @dev Verifies a Merkle proof proving the existence of a leaf in a Merkle tree. Assumes that each pair of leaves
-     * and each pair of pre-images are sorted.
-     * @param proof Merkle proof containing sibling hashes on the branch from the leaf to the root of the Merkle tree
-     * @param root Merkle root
-     * @param leaf Leaf of Merkle tree
-     * @return A boolean indicating whether the proof is valid or not.
-     */
+    /// @dev Verifies a Merkle proof proving the existence of a leaf in a Merkle tree. Assumes that each pair of leaves
+    ///      and each pair of pre-images are sorted.
+    /// @param proof Merkle proof containing sibling hashes on the branch from the leaf to the root of the Merkle tree
+    /// @param root Merkle root
+    /// @param leaf Leaf of Merkle tree
+    /// @return A boolean indicating whether the proof is valid or not.
     function _verify(bytes32[] calldata proof, bytes32 root, bytes32 leaf) internal pure returns (bool) {
         bytes32 computedHash = leaf;
 
@@ -744,23 +737,19 @@ contract RewardsDistributor {
         return computedHash == root;
     }
 
-    /**
-     * @dev Allows an address to claim a reward based on a provided nonce and Merkle proof.
-     * @param nonce A unique identifier for the reward claim, allowing multiple rewards to the same address.
-     * @param startTime The starting time from which the reward can be claimed.
-     * @param proof Merkle proof for validating the claim.
-     */
+    /// @dev Allows an address to claim a reward based on a provided nonce and Merkle proof.
+    /// @param nonce A unique identifier for the reward claim, allowing multiple rewards to the same address.
+    /// @param startTime The starting time from which the reward can be claimed.
+    /// @param proof Merkle proof for validating the claim.
     function claim(uint256 nonce, uint96 startTime, bytes32[] calldata proof) external {
         claimOnBehalf(msg.sender, nonce, startTime, proof);
     }
 
-    /**
-     * @dev Allows an address to claim rewards on behalf of another address based on a provided nonce and Merkle proof.
-     * @param onBehalf The address for which the rewards are being claimed.
-     * @param nonce A unique identifier for the reward claim, allowing multiple rewards to the same address.
-     * @param startTime The starting time from which the reward can be claimed.
-     * @param proof Merkle proof for validating the claim.
-     */
+    /// @dev Allows an address to claim rewards on behalf of another address based on a provided nonce and Merkle proof.
+    /// @param onBehalf The address for which the rewards are being claimed.
+    /// @param nonce A unique identifier for the reward claim, allowing multiple rewards to the same address.
+    /// @param startTime The starting time from which the reward can be claimed.
+    /// @param proof Merkle proof for validating the claim.
     function claimOnBehalf(address onBehalf, uint256 nonce, uint96 startTime, bytes32[] calldata proof) public {
         bytes32 node = keccak256(abi.encodePacked(onBehalf, nonce, startTime));
 
