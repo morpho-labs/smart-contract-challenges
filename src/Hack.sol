37e0039e9a1a90d0aa33da5e93215dfcc3a58092
makcandrov <makcandrov@proton.me>
2023-05-22 12:41:36 -0800

    refresh ex12

--- src/Hack.sol
+++ src/Hack.sol
@@ -384,46 +384,46 @@ contract Registry {
 
 /* Exercise 12 */
 
-/// @dev A Token contract that keeps a record of the users past balances.
-contract SnapShotToken {
+/// @dev A Token contract that keeps a record of the user's past balances.
+contract SnapshotToken {
     mapping(address => uint256) public balances;
-    mapping(address => mapping(uint256 => uint256)) public balanceAt;
+    mapping(address => mapping(uint256 => uint256)) public balancesAt;
 
     event BalanceUpdated(address indexed user, uint256 oldBalance, uint256 newBalance);
 
-    /// @dev Buys token at the price of 1ETH/token.
+    /// @dev Buys tokens at the price of 1 ETH per token.
     function buyToken() public payable {
-        uint256 _balance = balances[msg.sender];
-        uint256 _newBalance = _balance + msg.value / 1 ether;
-        balances[msg.sender] = _newBalance;
+        uint256 balance = balances[msg.sender];
+        uint256 newBalance = balance + msg.value / 1 ether;
+        balances[msg.sender] = newBalance;
 
-        _updateCheckpoint(msg.sender, _balance, _newBalance);
+        _updateCheckpoint(msg.sender, balance, newBalance);
     }
 
     /// @dev Transfers tokens.
-    /// @param _to The recipient.
-    /// @param _value The amount to send.
-    function transfer(address _to, uint256 _value) public {
-        uint256 _balancesFrom = balances[msg.sender];
-        uint256 _balancesTo = balances[_to];
+    /// @param to The recipient.
+    /// @param value The amount to send.
+    function transfer(address to, uint256 value) public {
+        uint256 oldBalanceFrom = balances[msg.sender];
+        uint256 oldBalanceTo = balances[to];
 
-        uint256 _balancesFromNew = _balancesFrom - _value;
-        balances[msg.sender] = _balancesFromNew;
+        uint256 newBalanceFrom = oldBalanceFrom - value;
+        balances[msg.sender] = newBalanceFrom;
 
-        uint256 _balancesToNew = _balancesTo + _value;
-        balances[_to] = _balancesToNew;
+        uint256 newBalanceTo = oldBalanceTo + value;
+        balances[to] = newBalanceTo;
 
-        _updateCheckpoint(msg.sender, _balancesFrom, _balancesFromNew);
-        _updateCheckpoint(_to, _balancesTo, _balancesToNew);
+        _updateCheckpoint(msg.sender, oldBalanceFrom, newBalanceFrom);
+        _updateCheckpoint(to, oldBalanceTo, newBalanceTo);
     }
 
-    /// @dev Records the users balance at this blocknumber
-    /// @param _user The address who's balance is updated.
-    /// @param _oldBalance The previous balance.
-    /// @param _newBalance The updated balance.
-    function _updateCheckpoint(address _user, uint256 _oldBalance, uint256 _newBalance) internal {
-        balanceAt[_user][block.timestamp] = _newBalance;
-        emit BalanceUpdated(_user, _oldBalance, _newBalance);
+    /// @dev Records the user's balance at this block number.
+    /// @param user The address whose balance is updated.
+    /// @param oldBalance The previous balance.
+    /// @param newBalance The updated balance.
+    function _updateCheckpoint(address user, uint256 oldBalance, uint256 newBalance) internal {
+        balancesAt[user][block.timestamp] = newBalance;
+        emit BalanceUpdated(user, oldBalance, newBalance);
     }
 }
 
