b068f8da0783ba20f182b9f3f50386251c92c4a9
makcandrov <makcandrov@proton.me>
2023-05-22 12:26:27 -0800

    refresh ex11

--- src/Hack.sol
+++ src/Hack.sol
@@ -348,36 +348,37 @@ contract Resolver {
 /* Exercise 11*/
 
 /// @dev Contract for users to register. It will be used by other contracts to attach rights to those users (rights will be linked to user IDs).
-///      Note that simply being registered does not confer any right.
+///      Note that simply being registered does not confer any rights.
 contract Registry {
     struct User {
-        address payable regAddress;
+        address account;
         uint64 timestamp;
-        bool registered;
         string name;
         string surname;
         uint256 nonce;
     }
 
     // Nonce is used so the contract can add multiple profiles with the same first name and last name.
-    mapping(string => mapping(string => mapping(uint256 => bool))) public isRegistered; // name -> surname -> nonce -> registered/not registered.
-    mapping(bytes32 => User) public users; // User isn't identified by address but by his ID, since the same person can have multiple addresses.
+    mapping(string => mapping(string => mapping(uint256 => bool))) public isRegistered;
+    // Users aren't identified by address but by their IDs, since the same person can have multiple addresses.
+    mapping(bytes32 => User) public users;
 
     /// @dev Adds yourself to the registry.
-    /// @param _name The first name of the user.
-    /// @param _surname The last name of the user.
-    /// @param _nonce An arbitrary number to allow multiple users with the same first and last name.
-    function register(string calldata _name, string calldata _surname, uint256 _nonce) public {
-        require(!isRegistered[_name][_surname][_nonce], "This profile is already registered");
-        isRegistered[_name][_surname][_nonce] = true;
-        bytes32 ID = keccak256(abi.encodePacked(_name, _surname, _nonce));
-        User storage user = users[ID];
-        user.regAddress = payable(msg.sender);
-        user.timestamp = uint64(block.timestamp);
-        user.registered = true;
-        user.name = _name;
-        user.surname = _surname;
-        user.nonce = _nonce;
+    /// @param name The first name of the user.
+    /// @param surname The last name of the user.
+    /// @param nonce An arbitrary number to allow multiple users with the same first and last name.
+    function register(string calldata name, string calldata surname, uint256 nonce) public {
+        require(!isRegistered[name][surname][nonce], "This profile is already registered.");
+        isRegistered[name][surname][nonce] = true;
+        bytes32 id = keccak256(abi.encodePacked(name, surname, nonce));
+
+        users[id] = User({
+            account: msg.sender,
+            timestamp: uint64(block.timestamp),
+            name: name,
+            surname: surname,
+            nonce: nonce
+        });
     }
 }
 
