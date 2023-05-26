2f6422687a7c3a184a1db11901301466573fef91
makcandrov <makcandrov@proton.me>
2023-05-25 17:48:14 -0800

    ex2: rename objectBought => objectsBought

--- src/Hack.sol
+++ src/Hack.sol
@@ -42,19 +42,19 @@ contract Store {
 /// @dev Enables users to buy objects at discounted prices.
 contract DiscountedBuy {
     uint256 public constant BASE_PRICE = 1 ether;
-    mapping(address => uint256) public objectBought;
+    mapping(address => uint256) public objectsBought;
 
     /// @dev Allows a user to buy an object by paying the appropriate price.
-    /// @notice The price is calculated as `BASE_PRICE / (1 + objectBought[msg.sender])`.
+    /// @notice The price is calculated as `BASE_PRICE / (1 + objectsBought[msg.sender])`.
     function buy() external payable {
-        require(msg.value * (1 + objectBought[msg.sender]) == BASE_PRICE, "Incorrect payment amount");
-        objectBought[msg.sender]++;
+        require(msg.value * (1 + objectsBought[msg.sender]) == BASE_PRICE, "Incorrect payment amount");
+        objectsBought[msg.sender]++;
     }
 
     /// @dev Calculates and returns the price of the next object to be purchased.
     /// @return The amount to be paid in wei.
     function price() external view returns (uint256) {
-        return BASE_PRICE / (1 + objectBought[msg.sender]);
+        return BASE_PRICE / (1 + objectsBought[msg.sender]);
     }
 }
 
